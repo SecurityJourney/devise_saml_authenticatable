9a79ff1204ba78171dac474d651f1746ef38942c
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2020-01-17 20:50:29 -0500

    avoid named route collisions

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -12,9 +12,9 @@ ActionDispatch::Routing::Mapper.class_eval do
       end
     else
       resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
-        get :new, path: 'saml/sign_in', as: 'new'
+        get :new, path: 'saml/sign_in', as: 'new_saml'
         post :create, path: 'saml/auth'
-        match :destroy, path: mapping.path_names[:sign_out], as: 'destroy', via: mapping.sign_out_via
+        match :destroy, path: mapping.path_names[:sign_out], as: 'saml_destroy', via: mapping.sign_out_via
         get :metadata, path: 'saml/metadata'
         match :idp_sign_out, path: 'saml/idp_sign_out', via: [:get, :post]
       end
